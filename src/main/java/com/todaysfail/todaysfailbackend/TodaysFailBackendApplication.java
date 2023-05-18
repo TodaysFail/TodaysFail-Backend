392ae8d9e5e73ddbdbd3bdd0c3264862f2ec1259
kdomo <dongho5088@naver.com>
2023-05-18 20:24:04 +0900

    init: project initial

---
+++ src/main/java/com/todaysfail/todaysfailbackend/TodaysFailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todaysfail.todaysfailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysFailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysFailBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaysfail/todaysfailbackend/TodaysFailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaysfail.todaysfailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysFailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
