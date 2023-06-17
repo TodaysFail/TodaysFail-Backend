061cdfb4aa1625dfa053e4440a3278b126c88b0b
kdomo <dongho5088@naver.com>
2023-06-17 21:34:58 +0900

    [#5] chore: 실행가능한 Application과 contextLoads 테스트 생성

---
+++ TodaysFail-Interface/src/main/java/com/todaysfail/TodaysFailApplication.java
@@ -0,0 +1,10 @@
+package com.todaysfail;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysFailApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(TodaysFailApplication.class, args);
+    }
+}
