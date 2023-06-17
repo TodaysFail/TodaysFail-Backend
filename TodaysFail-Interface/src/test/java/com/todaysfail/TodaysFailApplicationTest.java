89d4a6fbf7f6f32693d097077edc3173f795f347
kdomo <dongho5088@naver.com>
2023-06-17 21:34:58 +0900

    [#5] chore: 실행가능한 Application과 contextLoads 테스트 생성

---
+++ TodaysFail-Interface/src/test/java/com/todaysfail/TodaysFailApplicationTest.java
@@ -0,0 +1,11 @@
+package com.todaysfail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysFailApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
