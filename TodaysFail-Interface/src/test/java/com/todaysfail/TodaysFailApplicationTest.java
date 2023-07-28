20299c312af50a33d1e5958a21674d966986f6d9
kdomo <dongho5088@naver.com>
2023-07-28 21:55:19 +0900

    [#26] Interface 모듈 테스트 환경 구축

--- TodaysFail-Interface/src/test/java/com/todaysfail/TodaysFailApplicationTest.java
+++ TodaysFail-Interface/src/test/java/com/todaysfail/TodaysFailApplicationTest.java
@@ -1,9 +1,8 @@
 package com.todaysfail;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@InterfaceIntegrateSpringBootTest
 class TodaysFailApplicationTest {
 
     @Test
