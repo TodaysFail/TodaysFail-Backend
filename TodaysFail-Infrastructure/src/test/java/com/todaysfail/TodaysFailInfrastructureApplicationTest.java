7250c95083b25fac66324e780956a074c8039177
kdomo <dongho5088@naver.com>
2023-07-28 21:51:29 +0900

    [#26] infra 모듈 테스트 환경 구축

---
+++ TodaysFail-Infrastructure/src/test/java/com/todaysfail/TodaysFailInfrastructureApplicationTest.java
@@ -0,0 +1,11 @@
+package com.todaysfail;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+@InfraIntegrateSpringBootTest
+class TodaysFailInfrastructureApplicationTest {
+    @Test
+    void contextLoads() {}
+}
