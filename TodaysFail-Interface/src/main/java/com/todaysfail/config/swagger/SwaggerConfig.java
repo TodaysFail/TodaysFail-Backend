1bcc564afea6df604e1d0bb6791ab5dec662bd6b
도모 <dongho5088@naver.com>
2023-08-10 11:09:45 +0900

    [#51] chore: Swagger Config에 버전과 서버 목록 추가 (#53)

--- TodaysFail-Interface/src/main/java/com/todaysfail/config/swagger/SwaggerConfig.java
+++ TodaysFail-Interface/src/main/java/com/todaysfail/config/swagger/SwaggerConfig.java
@@ -1,29 +1,63 @@
 package com.todaysfail.config.swagger;
 
+import static com.todaysfail.common.consts.TodaysFailConst.*;
+
+import com.todaysfail.common.helper.SpringEnvironmentHelper;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+    private static final String SERVER_NAME = "TodaysFail";
+    private static final String API_TITLE = "TodaysFail API 문서";
+    private static final String API_DESCRIPTION = "TodaysFail API 문서입니다.";
+    private static final String GITHUB_URL = "https://github.com/TodaysFail/TodaysFail-Backend";
+
+    private final SpringEnvironmentHelper springEnvironmentHelper;
+
+    @Value("${springdoc.swagger-ui.version}")
+    private String swaggerVersion;
+
     @Bean
     public OpenAPI openAPI() {
-        Info info =
-                new Info()
-                        .title("오늘의 실패 API 문서")
-                        .description("오늘의 실패 API 문서입니다.")
-                        .version("v0.0.1");
-        return new OpenAPI().info(info);
+        return new OpenAPI().servers(swaggerServers()).info(swaggerInfo());
+    }
+
+    private List<Server> swaggerServers() {
+        Server server = new Server();
+        String serverUrl = getServerUrl();
+        server.url(serverUrl);
+        server.description(API_DESCRIPTION);
+        return List.of(server);
+    }
+
+    private String getServerUrl() {
+        switch (springEnvironmentHelper.getCurrentProfile()) {
+            case "prod":
+                return PROD_SERVER_URL;
+            case "dev":
+                return DEV_SERVER_URL;
+            default:
+                return LOCAL_SERVER_URL;
+        }
     }
 
     private Info swaggerInfo() {
         License license = new License();
-        license.url("https://github.com/TodaysFail/TodaysFail-Backend");
-        license.setName("TodaysFail");
+        license.url(GITHUB_URL);
+        license.setName(SERVER_NAME);
         return new Info()
-                .title("TodaysFail API 문서")
-                .description("TodaysFail API 문서입니다.")
-                .version("v0.0.1")
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(swaggerVersion)
                 .license(license);
     }
 }
