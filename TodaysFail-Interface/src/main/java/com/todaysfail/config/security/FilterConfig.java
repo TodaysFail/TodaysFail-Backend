474d5be4ae584273c1c0196b6bdc085adb1271c9
도모 <dongho5088@naver.com>
2023-08-18 22:44:05 +0900

    [#79] feat: accessDeniedHandler 적용 (#80)

--- TodaysFail-Interface/src/main/java/com/todaysfail/config/security/FilterConfig.java
+++ TodaysFail-Interface/src/main/java/com/todaysfail/config/security/FilterConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,12 @@ public class FilterConfig
         extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final JwtTokenFilter jwtTokenFilter;
     private final JwtExceptionFilter jwtExceptionFilter;
+    private final AccessDeniedFilter accessDeniedFilter;
 
     @Override
     public void configure(HttpSecurity builder) {
         builder.addFilterBefore(jwtTokenFilter, BasicAuthenticationFilter.class);
         builder.addFilterBefore(jwtExceptionFilter, JwtTokenFilter.class);
+        builder.addFilterBefore(accessDeniedFilter, FilterSecurityInterceptor.class);
     }
 }
