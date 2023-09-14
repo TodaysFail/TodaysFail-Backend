d35b23c9f7f0c96d3321460145ed1747b0b461e5
kdomo <dongho5088@naver.com>
2023-09-14 21:55:27 +0900

    [#110] test: HealthCheck Test Code

---
+++ TodaysFail-Interface/src/test/java/com/todaysfail/api/web/example/ExampleControllerTest.java
@@ -0,0 +1,22 @@
+package com.todaysfail.api.web.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ExampleController.class)
+@ContextConfiguration(classes = ExampleController.class)
+class ExampleControllerTest {
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    void healthCheck() throws Exception {
+        mockMvc.perform(get("/api/v1/examples/health")).andExpect(status().isOk());
+    }
+}
