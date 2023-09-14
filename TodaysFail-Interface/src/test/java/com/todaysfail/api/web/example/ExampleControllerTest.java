db3a2c70a87d750d87858118f8eed88ac0b87018
kdomo <dongho5088@naver.com>
2023-09-14 21:59:36 +0900

    [#110] test: AutoConfigureMockMvc addFilters false 속성 추가

--- TodaysFail-Interface/src/test/java/com/todaysfail/api/web/example/ExampleControllerTest.java
+++ TodaysFail-Interface/src/test/java/com/todaysfail/api/web/example/ExampleControllerTest.java
@@ -6,12 +6,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ExampleController.class)
 @ContextConfiguration(classes = ExampleController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ExampleControllerTest {
     @Autowired private MockMvc mockMvc;
 
